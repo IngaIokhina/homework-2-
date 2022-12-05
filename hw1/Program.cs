5b42f376fa42e3092181e33a8ccc35cd5833b87e
Inga Iokhina <iiokhina95@mail.ru>
2022-12-05 13:54:44 +0400

    add hm1

---
+++ hw1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+
+ */
+
+Console.WriteLine("Enter number");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+int hundreds = number % 100;
+int secondNumber = hundreds / 10; 
+
+Console.WriteLine($"Result is {secondNumber}");
+
+
