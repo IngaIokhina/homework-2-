0e6fc59813113930468528ded0fd06b50820a995
Inga Iokhina <iiokhina95@mail.ru>
2022-12-05 14:57:49 +0400

    add hm2

---
+++ hw2/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+
+PS. Я гуглила такую штуку + result[..] - так как сама не догодалась :((
+*/
+
+
+Console.Write("Enter the number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+string result = Convert.ToString(number);
+
+if (result.Length > 2)
+{
+  Console.WriteLine("third digit  " + result[2]);
+}
+else 
+{
+  Console.WriteLine("no third digit");
+}
